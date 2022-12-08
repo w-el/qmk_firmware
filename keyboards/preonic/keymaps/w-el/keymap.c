2d2b34e7a3feb5081342f968a2183aee823a3e5c
w-el <waynehlee@gmail.com>
2022-12-08 12:04:38 +0800

    Feature added[Production ready]: adds comment in the excel saying which macro was run. Helps in cases where you may run the wrong macro.

--- keyboards/preonic/keymaps/w-el/keymap.c
+++ keyboards/preonic/keymaps/w-el/keymap.c
@@ -35,6 +35,7 @@ enum preonic_keycodes {
   BACKLIT,
   HTN,
   LINE,
+  DM,
   TEST
 };
 
@@ -88,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |  HTN |   J  |   K  |   L  |   ;  |  "   |
+ * | Esc  |   A  |   S  |  DM  |   F  |   G  |  HTN |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -98,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, TEST,    _______, _______, _______, _______, _______, _______,
-  KC_1,    _______, _______, _______, _______, _______, HTN,     _______, _______, LINE,    _______, _______,
+  KC_1,    _______, _______, DM,      _______, _______, HTN,     _______, _______, LINE,    _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
@@ -181,7 +182,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             //Switch to epic and open orders only enounter
             SS_LGUI("2") SS_DELAY(100) SS_LCTL("w") SS_DELAY(5000) SS_LCTL("5")
             //Fill in the details for orders only encounter
-            //this next line is  tabbing to the CSN x10
+            //this next line is  tabbing to the CSN x10¸
             SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) //Clinic
             SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB)
             //SS_LSFT(SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB)) //Corp laptop
@@ -206,6 +207,61 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             //MOVING BACK TO EXCEL AND SORTING THE FILE
             //switch to excel
             SS_LGUI("m") SS_LGUI("2") SS_DELAY(1000) SS_LGUI("1") SS_DELAY(1000) SS_TAP(X_ESC) SS_DELAY(100)
+            //add comment of what lab it is
+            SS_TAP(X_RGHT) "HTN panel"
+            //shift cursor back to left most cell
+            SS_LCTL(SS_TAP(X_LEFT)) SS_DELAY(100)
+            // //select row
+            SS_LSFT(SS_TAP(X_SPC))
+            //highlight row
+            SS_LALT("h") SS_DELAY(100) "h" SS_TAP(X_UP) SS_TAP(X_UP) SS_TAP(X_UP) SS_DELAY(100) SS_TAP(X_ENTER) SS_DELAY(100)
+            //move down cell to next row
+            SS_TAP(X_DOWN)
+            //save the file
+            SS_LCTL("s")
+
+
+
+            );
+          }
+          break;
+        case DM:
+          if (record->event.pressed)
+          {
+            SEND_STRING(
+            //set win 1 as excel window and win 2 as the epic window
+            //In excel: highlight cell of patient UID
+            SS_LCTL("c") SS_DELAY(100) SS_LGUI("m") SS_DELAY(100)
+            //Switch to epic and open orders only enounter
+            SS_LGUI("2") SS_DELAY(100) SS_LCTL("w") SS_DELAY(5000) SS_LCTL("5")
+            //Fill in the details for orders only encounter
+            //this next line is  tabbing to the CSN x10¸
+            SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) //Clinic
+            SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB)
+            //SS_LSFT(SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB) SS_TAP(X_TAB)) //Corp laptop
+
+            // Pasting in the CSN and then opening the orders only encounter
+            SS_DELAY(1000) SS_LCTL("v") SS_DELAY(1000) SS_LALT("fs") SS_DELAY(1000)
+            //switching back to excel to copy the date
+            SS_LGUI("m") SS_DELAY(100) SS_LGUI("1") SS_DELAY(100) SS_TAP(X_ESC) SS_DELAY(100) SS_LCTL(SS_TAP(X_RGHT)) SS_DELAY(100) SS_LCTL("c") SS_DELAY(5000)
+            //switch back to epic and open up orders
+            SS_LGUI("m") SS_DELAY(100) SS_LGUI("2") SS_DELAY(1000) SS_LCTL("o") SS_DELAY(1000)
+
+            //at order specific section
+            "diabetes panel" SS_DELAY(200) SS_TAP(X_ENTER) SS_DELAY(2000)
+            SS_LALT("f") SS_DELAY(2000) SS_TAP(X_ENTER) SS_DELAY(4000)
+            SS_LCTL("v") //paste back the date
+            SS_DELAY(2000)
+
+            //sending order
+            SS_LALT("s") SS_DELAY(1000)
+            SS_LCTL(SS_LSFT("e")) SS_DELAY(5000)
+
+            //MOVING BACK TO EXCEL AND SORTING THE FILE
+            //switch to excel
+            SS_LGUI("m") SS_LGUI("2") SS_DELAY(1000) SS_LGUI("1") SS_DELAY(1000) SS_TAP(X_ESC) SS_DELAY(100)
+            //add comment of what lab it is
+            SS_TAP(X_RGHT) "DM panel"
             //shift cursor back to left most cell
             SS_LCTL(SS_TAP(X_LEFT)) SS_DELAY(100)
             // //select row
